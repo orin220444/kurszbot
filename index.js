be049cf5653e9bce1304bfa758e5652af727f527
Orin Serimon <orin220444@protonmail.com>
2020-01-22 01:45:19 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const Telegraf = require('telegraf');
+const request = require('request');
+const schedule = require('node-schedule');
+require('dotenv').config({path: './.env'});
+const bot = new Telegraf(process.env.BOT_TOKEN);
+bot.hears('курс', (ctx) => {
+  schedule.scheduleJob({hour: 11}, function() {
+    request('https://www.cbr-xml-daily.ru/daily_json.js', (error, response, body) => {
+      if (error) throw new Error(Error);
+      if (response.statusCode === 200) {
+        const data = JSON.parse(body);
+        ctx.reply(`Курс валют относительно к рублю на данный момент:
+    Доллар: ${data.Valute.USD.Value} ,
+    Евро: ${data.Valute.EUR.Value},
+    Фунт стерлингов: ${data.Valute.GBP.Value},
+    Юань: ${data.Valute.CNY.Value}.
+    `);
+      }
+    });
+  });
+});
+bot.launch();
