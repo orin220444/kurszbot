91b5eb39906b8e813f325351f34ce596c54963b9
Orin Serimon <orin220444@protonmail.com>
2020-04-25 11:21:15 +0700

    use axios instead of request

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Telegraf = require('telegraf');
-const request = require('request');
+const axios = require('axios');
 const schedule = require('node-schedule');
 require('dotenv').config({path: './.env'});
 const bot = new Telegraf(process.env.BOT_TOKEN);
@@ -7,21 +7,21 @@ const hour = process.env.HOUR_TO_SEND;
 const minute = process.env.MINUTE_TO_SEND;
 bot.hears('курс', (ctx) => {
   schedule.scheduleJob({hour: hour, minute: minute}, function() {
-    request('https://www.cbr-xml-daily.ru/daily_json.js', (error, response, body) => {
-      if (error) throw new Error(Error);
-      if (response.statusCode === 200) {
-        let data = JSON.parse(body);
+    axios.get('https://www.cbr-xml-daily.ru/daily_json.js')
+      .then(function(response){
+        let data = response.data.Valute
         ctx.telegram.sendMessage(process.env.CHANNEL_ID,
             `Курс валют относительно к рублю на данный момент:
-  Доллар: ${data.Valute.USD.Value},
-  Евро: ${data.Valute.EUR.Value},
-  Фунт стерлингов: ${data.Valute.GBP.Value},
-  Юань: ${data.Valute.CNY.Value}.
+  Доллар: ${data.USD.Value},
+  Евро: ${data.EUR.Value},
+  Фунт стерлингов: ${data.GBP.Value},
+  Юань: ${data.CNY.Value}.
   `);
         ctx.reply('Курс валют уже на канале!');
-      }
+        
+      })
     });
-  });
+
   ctx.reply(
       `Сообщение с курсом валют будет отправлено в ${hour}:${minute}`,
   );
